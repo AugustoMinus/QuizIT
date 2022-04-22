5fe49743c7c0490202e21706f0628c401049d575
augusto minus <augustominuscoli@gmail.com>
2022-04-22 20:54:45 -0300

    Initial commit

--- app/src/main/java/com/augusto/myapplication/MainActivity.java
+++ app/src/main/java/com/augusto/myapplication/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.activity_home);
 
 
 
---
+++ app/src/main/java/com/augusto/myapplication/resultadoquiz.java
@@ -0,0 +1,6 @@
+package com.augusto.myapplication;
+
+import android.app.Activity;
+
+public class resultadoquiz extends Activity {
+}
